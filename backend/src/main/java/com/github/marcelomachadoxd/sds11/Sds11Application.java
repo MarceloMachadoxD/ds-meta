6dd0c1313cc057a075af5d6550d9aafaa2217ce2
MarceloMachadoxD <infomachado1987@gmail.com>
2022-11-14 11:05:49 -0300

    Project backend created

---
+++ backend/src/main/java/com/github/marcelomachadoxd/sds11/Sds11Application.java
@@ -0,0 +1,13 @@
+package com.github.marcelomachadoxd.sds11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sds11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sds11Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/marcelomachadoxd/sds11/Sds11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcelomachadoxd.sds11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sds11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
